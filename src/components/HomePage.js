d5f86013c064efadeeedcad75a634d6023fd23b3
arhumkhan123 <arhumrocket@gmail.com>
2023-01-21 16:04:42 -0500

    HomePae file added

---
+++ src/components/HomePage.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div className="container bg-light  mt-4 p-4">
+                <div className="row">
+                    <div className="container card mt-4 p-4">
+                        <div className="col-6 align-self-center">
+                            <h2> Welcome </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+
+    }
+}

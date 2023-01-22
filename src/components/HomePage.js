02e6295e4b5281502ddb95104a1a4dfcaca70403
arhumkhan123 <arhumrocket@gmail.com>
2023-01-22 12:56:01 -0500

    changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,7 @@
 import { Component } from "react";
+import {Button, Row} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap/lib/Tab";
 
 export default class HomePage extends Component {
     render() {
@@ -12,18 +15,23 @@ export default class HomePage extends Component {
                     <div className="text-left"></div>
                     
                 </div>
+                <div className="row px-4 my-4">
 
 
-                            <div className="container card mt-4" >
+                            
                             <h4>GetInLine Inc. is a queue managament and alert system that calls you to scan a QR code at your doctors office, sign in and go 
                             do what you would like with your time until your time is up. </h4>
-                            <div class = "text-left">
-                                <div class = "card-body">
-                                    <button class = "btn-primary rounded-pill">Member</button>
-                                    <button class = "btn-primary rounded-pill text-center">Host</button>
+                            <div className="row my-3 text-left">
+                                <div className="col sm = {99}">
+                                <button class = "btn-primary  btn-lg">Member</button>
+                                    </div>
+                                    <div className="col sm = {80}">
+                                    <button class = "btn-primary btn-blocj text-center btn-lg">Host</button>
+                                    </div>
 
+                                
                                 </div>
-                            </div>
+                            
                             
                     </div>
 

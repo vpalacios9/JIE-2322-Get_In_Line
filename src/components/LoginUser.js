61bc98acb6db3017fbc8745ea04a3210fc328a80
Valeria Palacios <vpalacios9@gatech.edu>
2023-03-11 16:34:35 -0500

    feedback messages login

--- src/components/LoginUser.js
+++ src/components/LoginUser.js
@@ -6,11 +6,15 @@ import { auth } from '../firebaseConfig';
 const LoginUser = () => {
     const navigate = useNavigate();
     const [user, setUser] = useState({});
+    const [errorMessage, setErrorMessage] = useState(null);
 
     const submit = (user) => {
         signInWithEmailAndPassword(auth, user.email, user.password)
             .then(() => {
                 navigate("/userSelectPage")
+            })
+            .catch((error) => {
+                setErrorMessage(error.message);
             });
     }
 
@@ -24,48 +28,49 @@ const LoginUser = () => {
 
     return (
         <div className="container bg-light  mt-4 p-4">
-        <div className= "row" >
-            <div className="container card mt-4 p-4">
-                <div className="text-center">
-                    <h4>Login</h4>
-                </div>
-                <div className="text-left">
-                    <div className="card-body">
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Email address</label>
-                            <input
-                                type="email"
-                                class="form-control"
-                                id="exampleFormControlInput1"
-                                placeholder="name@example.com"
-                                onChange={(e) => onChange("email", e.target.value)}
-                            />
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" 
-                                onChange={(e) => onChange("password", e.target.value)}
-                            />
-                        </div>
+            <div className= "row" >
+                <div className="container card mt-4 p-4">
+                    <div className="text-center">
+                        <h4>Login</h4>
                     </div>
-                    <div class="col text-center">
-                        <button
-                            type="submit"
-                            class="btn btn-primary"
-                            onClick={() => submit(user)}
-                        >
-                            Sign in
-                        </button>
-                        <br/>
-                        <br/>
-                        <button class ="text-center, btn btn-secondary" onClick={() => navigate("/adminsignup")}>Create new Host</button>
-                        &nbsp; &nbsp;
-                        <button class="text-center, btn  btn-secondary" onClick={() => navigate("/usersignup")}>Create new User</button>
+                    <div className="text-left">
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label for="exampleFormControlInput1">Email address</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="exampleFormControlInput1"
+                                    placeholder="name@example.com"
+                                    onChange={(e) => onChange("email", e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" 
+                                    onChange={(e) => onChange("password", e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+                        <div className="col text-center">
+                            <button
+                                type="submit"
+                                className="btn btn-primary"
+                                onClick={() => submit(user)}
+                            >
+                                Sign in
+                            </button>
+                            <br/>
+                            <br/>
+                            <button className="text-center, btn btn-secondary" onClick={() => navigate("/adminsignup")}>Create new Host</button>
+                            &nbsp; &nbsp;
+                            <button className="text-center, btn btn-secondary" onClick={() => navigate("/usersignup")}>Create new User</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 

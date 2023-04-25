fda51cbcdec89ccf47106e47be587fecfbb34e15
arhumkhan123 <arhumrocket@gmail.com>
2023-04-24 20:25:51 -0400

    documentation

--- src/components/MyQueues.js
+++ src/components/MyQueues.js
@@ -6,57 +6,37 @@ import { Container } from "react-bootstrap/lib/Tab";
 import Navbar from "./Navbar";
 import { auth, database } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
-import {collection,query,onSnapshot,where,getDoc, doc,} from "firebase/firestore";
-
+import {collection,query,onSnapshot,where,getDoc, doc,} from "firebase/firestore"; //imports firebase functions 
 const MyQueues = () => {
-  const [queues, setQueues] = useState([]);
+  const [queues, setQueues] = useState([]); //defines the queues state variable with setQueues to use the useState hook 
   const navigate = useNavigate();
-  const currentUser = auth.currentUser;
-
-
+  const currentUser = auth.currentUser; //makes sure that the current user logged in is authenitcated 
   useEffect(() => {
-    if (currentUser) {
-      const userDocRef = doc(database, "users", currentUser.uid);
-  
-      const unsubscribe = onSnapshot(userDocRef, async (userDoc) => {
-       
-          const data = userDoc.data();
-          const userQueues = data.queues;
-  
-          console.log('User Queues:', userQueues);
-  
-          
-            const queuesData = [];
-  
-            for (const queueId of userQueues) {
-              const queueDoc = await getDoc(doc(database, "queue", queueId));
-              queuesData.push({ id: queueId, name: queueDoc.data().name });
+    if (currentUser) { //checks to see if user is authenciated, and then it will execute the code 
+      const userDocRef = doc(database, "users", currentUser.uid); //creates a reference tothe firebase document in the database. takes in the database and the users collection as an argument.
+      const unsubscribe = onSnapshot(userDocRef, async (userDoc) => {    //listener to see real time updates that may happen on Firestore 
+          const data = userDoc.data(); //gets data from the userdoc document and stores it in data 
+          const userQueues = data.queues; //gets the queues field from data and sets it equal to the userQueues variable 
+          console.log('User Queues:', userQueues); //logs userQueues value 
+            const queuesData = []; //initializes array to store queue data 
+            for (const queueId of userQueues) { //iteratres through queueId  in the userQueues array 
+              const queueDoc = await getDoc(doc(database, "queue", queueId));  //gets the queueId document 
+              queuesData.push({ id: queueId, name: queueDoc.data().name }); //pushes retrieved data to array
             }
-  
-            
-  
             setQueues(queuesData);
-           
-         
       });
-  
       return () => {
         unsubscribe();
       };
     } 
-  }, [currentUser]);
-  
-  
- 
+  }, [currentUser]); 
   if (!currentUser) {
     alert("user not logged in")
     navigate('/');
   }
-
   const handleClick = () => {
     navigate('/WaitTime');
   };
-
   return (
     <div>
       <Navbar />
